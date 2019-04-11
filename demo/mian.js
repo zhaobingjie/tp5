6d55eaab2a15fd0babf19a75407ec660bbb4fda5
zhaobingjie <18222939377@163.com>
2019-04-11 23:38:45 +0800

    eventEmitter

--- demo/mian.js
+++ demo/mian.js
@@ -19,22 +19,28 @@ var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
 // 创建事件处理程序
-var connectHandler = function connected() {
-   console.log('连接成功。');
+// var connectHandler = function connected() {
+//    console.log('连接成功。');
   
-   // 触发 data_received 事件 
-   eventEmitter.emit('data_received');
-}
+//    // 触发 data_received 事件 
+//    eventEmitter.emit('data_received');
+// }
 
-// 绑定 connection 事件处理程序
-eventEmitter.on('connection', connectHandler);
+// // 绑定 connection 事件处理程序
+// eventEmitter.on('connection', connectHandler);
  
-// 使用匿名函数绑定 data_received 事件
-eventEmitter.on('data_received', function(){
-   console.log('数据接收成功。');
-});
+// // 使用匿名函数绑定 data_received 事件
+// eventEmitter.on('data_received', function(){
+//    console.log('数据接收成功。');
+// });
 
-// 触发 connection 事件 
-eventEmitter.emit('connection');
+// // 触发 connection 事件 
+// eventEmitter.emit('connection');
 
-console.log("程序执行完毕。");
+// console.log("程序执行完毕。");
+
+var demoHandler = function demo(){
+	console.log('demo is runing');
+}
+eventEmitter.on('runDemo',demoHandler);
+eventEmitter.emit('runDemo');
