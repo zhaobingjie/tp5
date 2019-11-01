d5aebad17d98a25c75d7ec776ba4d192b087c91f
jack <18222939377@163.com>
2019-11-01 18:02:20 +0800

    restart

--- demo1/module_a.js
+++
@@ -1,6 +0,0 @@
-var user = 'jack';
-function getUser(){
-    console.log(user);
-}
-exports.api_getUser = getUser;
-exports.api_name = user;
--- demo2/superagent.js
+++
@@ -1,4 +0,0 @@
-var request = require('superagent');
-request.get('http://localhost:3000/search').send({q:'jack'}).end(function(res){
-    console.log(res.text);
-})
